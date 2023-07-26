ff83b3e61d91203b6bdfb54f55c5e0b3dfa59edc
JAMBITI <biti.jamal@gmail.com>
2023-07-26 13:57:53 -0700

    simpleshell

---
+++ head.h
@@ -0,0 +1,111 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <fcntl.h>
+
+/*Global Variables  */
+extern char **environ;
+extern int EXIT_STATUS;
+
+/**
+ * struct AliasStruct - struct aliases
+ * @name: alias name
+ * @value: alias value
+ */
+typedef struct AliasStruct
+{
+char *name;
+char *value;
+} AliasStruct;
+
+/* Custom Memory Functions */
+void *custom_calloc(unsigned int num_elements, unsigned int element_size);
+void custom_free(char **ptr);
+
+ /*String Manipulation Functions  */
+char *custom_strcpy(char *destination, char *source);
+char *custom_strcat(char *destination, char *source);
+void remove_first_char(char **string_ptr);
+size_t my_strlen(char *str);
+int custom_strcmp(const char *str1, char *str2);
+int my_strncmp(const char *str1, char *str2, int num_chars);
+char *find_substring(char *str, char *sub);
+char *my_memcpy(char *destination, char *source, unsigned int num_bytes);
+void replace_custom_substring(char *input_str, char *old_sub, char *new_sub);
+
+/* String Tokenization Function */
+char **custom_strtok(char *input_string, char *delimiters);
+
+/* Environment Functions */
+char *custom_getenv(char *variable);
+
+/* Command Execution Functions */
+int custom_fork(char *cmd_name, char **cmd_args);
+int execute_command(char **args, char *program_name, char *program_path);
+int check_fork_error(char *program_name, char **args, char *program_path);
+
+/* I/O and File Handling Functions */
+int read_line(char **user_input);
+int read_file(char **user_input, char **file_args);
+int custom_to_string(int output_fd, unsigned int number);
+int custom_print_string(int output_fd, char *str);
+void display_error(char **arguments, char *file_path, int error_code);
+int report_error(char *cmd_name, char **cmd_args,
+		char *program_name, int error_code);
+
+/* Command Parsing Functions */
+int split_command(char *command, char *program_name,
+		AliasStruct *aliases_list, int *index_ptr);
+void get_cmds(char ***, char *);
+int count_arguments(char **);
+char *is_valid_command(char *command, char *program_name, char **environment);
+char *check_ops(char **);
+
+/*Environment Variable Functions  */
+int custom_export(int arg_count, char **arguments, char *program_name,
+AliasStruct *custom_alias,
+int *index_ptr, char **custom_split, char *custom_line);
+int custom_unsetenv(int count, char **arguments, char *programName,
+MyDataStructure *data, int *index, char **splitLine, char *currentLine);
+
+/* Built-in Commands Functions */
+int my_echo(char **arguments);
+int custom_change_dir(int arg_count, char **arg_list, char *prog_name,
+AliasStruct *alias_struct, int *index_ptr,
+char **split_line, char *input_line);
+int my_exit_function(int num_args, char **arguments, char *program_name,
+AliasStruct *my_alias, int *index_value, char **split_line,
+char *input_line);
+int my_env_function(int count, char **arguments, char *program_name,
+AliasStruct *my_aliases, int *index_ptr, char **my_split, char *input_line);
+int handle_aliases(int count, char **arguments, char *program_name,
+AliasStruct *alias_list, int *alias_count,
+char **split_line, char *input_line);
+
+/**
+ * struct cmd_executer - structure
+ * @command: command
+ * @execute_func: function
+ */
+typedef struct cmd_executer
+{
+char *command;
+int (*execute_func)(int, char **, char *, AliasStruct *,
+int *, char **, char *);
+} cmd_executer;
+
+/* Command Selection Function */
+int select_command(const char *command, char **arguments,
+char *program_name, AliasStruct *alias_data,
+int *index, char **split_line, char *input_line);
+
+/* Utility Functions */
+int convert_string_to_integer(char *str);
+
+#endif
